c37d7d2e9f3690bcdde7df6a88fcdd0119693ac3
Pascal Guerineau <pascal.guerineau@ingenico.com>
2020-06-05 08:13:35 +0200

    Initial commit

---
+++ a1/a1.ino
@@ -0,0 +1,13 @@
+#define led 13
+
+void setup() {
+	pinMode(led, OUTPUT);
+}
+
+void loop() {
+	digitalWrite(led, HIGH);
+	delay(2000);
+	digitalWrite(led, LOW);
+	delay(2000);
+	analogWrite(led, );
+}
